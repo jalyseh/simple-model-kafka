0cd8674ea190feee5482e67584d940ad092c443f
jalyseh <hammettjadyn@gmail.com>
2021-01-06 14:21:39 -0500

    first commit

---
+++ src/main/java/com/example/kafka_prod_cons/KafkaConfig.java
@@ -0,0 +1,66 @@
+package com.example.kafka_prod_cons;
+
+import com.google.gson.Gson;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration //creates bean in start up
+@EnableKafka // allows spring app to listen to kafka events
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean //kafka template to perform producing sending to a topic via the app
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    //consumption of kafka topic
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory(){
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); //
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "myGroupId");
+
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new StringDeserializer());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+
+        return concurrentKafkaListenerContainerFactory;
+    }
+
+    @Bean //serialization bean for serialization & de-serialization
+    public Gson jsonConverter(){
+        return new Gson();
+    }
+
+}
